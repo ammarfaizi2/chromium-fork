23211963f8899d218170b5940c43e5a507d7b08e
Ammar Faizi <ammarfaizi2@gmail.com>
2022-01-14 14:26:30 +0700

    content_main_runner_impl: Add --hide-all command line argument
    We're going to add hide all patches that will be activated when a
    command line argument --hide-all is provided.
    
    This adds "hide-all" to the copied command line arguments.
    
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

--- content/app/content_main_runner_impl.cc
+++ content/app/content_main_runner_impl.cc
@@ -295,6 +295,7 @@ pid_t LaunchZygoteHelper(base::CommandLine* cmd_line,
       switches::kRegisterPepperPlugins,
       switches::kV,
       switches::kVModule,
+      "hide-all",
   };
   cmd_line->CopySwitchesFrom(*base::CommandLine::ForCurrentProcess(),
                              kForwardSwitches, base::size(kForwardSwitches));
