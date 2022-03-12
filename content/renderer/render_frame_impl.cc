ammarfaizi2/chromium-fork

commit 05b0e677a2ef4c5b4bcf0fa6fbe4d835ec5dbde2

task_g_026: Create NativeMakeEventTrusted() function
This will be the native function to be registered.

Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

content/renderer/render_frame_impl.cc
@@ -305,6 +305,28 @@ const char* get_browser_id(void);
 
 namespace content {
 
+void NativeMakeEventTrusted(const v8::FunctionCallbackInfo<v8::Value>& args) {
+  v8::Isolate* isolate = args.GetIsolate();
+  v8::HandleScope scope(isolate);
+  v8::Local<v8::Object> object;
+  blink::ScriptWrappable* wrapper;
+  blink::Event* event;
+
+  if (!args.Length()) {
+    isolate->ThrowException(v8::Exception::RangeError(
+      v8::String::NewFromUtf8(isolate, "Event is missing").ToLocalChecked()));
+    return;
+  }
+
+  object = v8::Local<v8::Object>::Cast(args[0]);
+  wrapper = blink::ToScriptWrappable(object);
+  event = wrapper->ToImpl<blink::Event>();
+
+  if (!event->isTrusted()) {
+    event->SetTrusted(true);
+  }
+}
+
 void NativeArrayMultiply(const v8::FunctionCallbackInfo<v8::Value>& args) {
   v8::Isolate* isolate = args.GetIsolate();
   v8::HandleScope scope(isolate);

commit 3f517ea6fa31ca5f968ce2f738f690278956f2aa

task_g_026: Sanitize malformed argument on NativeMakeEventTrusted()
When _makeEventTrusted() is called with an arbitrary type of argument,
it will crash the whole browser. Sanitize the argument, make sure it
has type of object with constructor name "PointerEvent". Throw a benign
JavaScript exception if the argument is malformed.

Fixes: 05b0e677a2ef4c5b4bcf0fa6fbe4d835ec5dbde2 ("task_g_026: Create NativeMakeEventTrusted() function")
Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

content/renderer/render_frame_impl.cc
@@ -257,6 +257,8 @@
 #include "content/renderer/java/gin_java_bridge_dispatcher.h"
 #endif
 
+#include <cstring>
+
 using base::Time;
 using blink::ContextMenuData;
 using blink::WebContentDecryptionModule;
@@ -310,20 +312,41 @@ void NativeMakeEventTrusted(const v8::FunctionCallbackInfo<v8::Value>& args) {
   v8::Local<v8::Object> object;
   blink::ScriptWrappable* wrapper;
   blink::Event* event;
+  const char *err_str;
 
-  if (!args.Length()) {
-    isolate->ThrowException(v8::Exception::RangeError(
-      v8::String::NewFromUtf8(isolate, "Event is missing").ToLocalChecked()));
-    return;
+  if (args.Length() != 1) {
+    err_str = "NativeMakeEventTrusted() function only accepts 1 argument";
+    goto err;
   }
+
+  if (!args[0]->IsObject())
+    goto err_wrong_type;
 
   object = v8::Local<v8::Object>::Cast(args[0]);
+
+  {
+    v8::String::Utf8Value tmp_str(isolate, object->GetConstructorName());
+    if (strcmp(*tmp_str, "PointerEvent"))
+      goto err_wrong_type;
+  }
+
+  /* Valid object here, do set trusted. */
   wrapper = blink::ToScriptWrappable(object);
   event = wrapper->ToImpl<blink::Event>();
-
-  if (!event->isTrusted()) {
+  if (!event->isTrusted())
     event->SetTrusted(true);
-  }
+
+  return;
+
+err_wrong_type:
+   err_str = "NativeMakeEventTrusted() function only accepts 1 argument with "
+             "type of PointerEvent";
+err:
+  isolate->ThrowException(
+    v8::Exception::RangeError(
+      v8::String::NewFromUtf8(isolate, err_str).ToLocalChecked()
+    )
+  );
 }
 
 void NativeArrayMultiply(const v8::FunctionCallbackInfo<v8::Value>& args) {