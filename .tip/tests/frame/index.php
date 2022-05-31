c088e0935a2d38236a0fe1f9d037ecba28247fb5
Ammar Faizi <ammarfaizi2@gmail.com>
2022-05-31 23:18:05 +0700

    .tip: Add frame tests
    Just for testing sub frame and main frame.
    
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

---
+++ .tip/tests/frame/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<iframe src="test_frame.php"></iframe>
+	<iframe src="test_frame.php"></iframe>
+	<iframe src="test_frame.php"></iframe>
+	<iframe src="test_frame.php"></iframe>
+	<iframe src="test_frame.php"></iframe>
+	<iframe src="test_frame.php"></iframe>
+	<iframe src="test_frame.php"></iframe>
+	<iframe src="test_frame.php"></iframe>
+	<iframe src="test_frame.php"></iframe>
+	<iframe src="test_frame.php"></iframe>
+	<iframe src="test_frame.php"></iframe>
+	<iframe src="test_frame.php"></iframe>
+</body>
+</html>
---
+++ .tip/tests/frame/test_frame.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Frame</title>
+</head>
+<body>
+	<h1>This is a sub frame.</h1>
+</body>
+</html>
